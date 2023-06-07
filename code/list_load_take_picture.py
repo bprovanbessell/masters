9bb3e75b9ed5af72dab4ac40f1c4d8cc96896c7e
Ben Provan-Bessell <benjaminlpb123@gmail.com>
2023-06-07 10:00:32 +0100

    Code to load objs, take a panorama, and remove them

---
+++ code/list_load_take_picture.py
@@ -0,0 +1,94 @@
+import bpy
+import glob
+import os
+from math import *
+from mathutils import *
+
+
+paths = ['/Users/bprovan/Downloads/dataset/148/textured_objs/*.obj']
+save_path = '/Users/bprovan/Desktop/blender_pics/'
+
+
+def load_objs(path_to_folder):
+
+    fn_list = glob.glob(path_to_folder)
+    for fn in fn_list:
+        
+        bpy.ops.import_scene.obj(filepath=fn, filter_glob='*.obj;*.mtl')
+
+def delete_objects():
+    # select all the objects
+    for obj in bpy.context.scene.objects:
+        if obj.type == 'MESH':
+            obj.select_set(True)
+        else:
+            obj.select_set(False)
+            
+    bpy.ops.object.delete()
+
+        #remove it, this way you dont need to reopen/close file...
+    for block in bpy.data.meshes:
+        if block.users == 0:
+            bpy.data.meshes.remove(block)
+
+    for block in bpy.data.materials:
+        if block.users == 0:
+            bpy.data.materials.remove(block)
+
+    for block in bpy.data.textures:
+        if block.users == 0:
+            bpy.data.textures.remove(block)
+
+    for block in bpy.data.images:
+        if block.users == 0:
+            bpy.data.images.remove(block)
+
+
+def take_panorama(instance_id, save_folder):
+
+    #set your own target here
+    # target = bpy.data.objects['new-0']
+    # there should be something loaded, if this throws an error, then we haven't loaded properly
+    mesh1 = bpy.data.meshes[0]
+    target = bpy.data.objects[mesh1]
+    cam = bpy.data.objects['Camera']
+    t_loc_x = target.location.x
+    t_loc_y = target.location.y
+    cam_loc_x = cam.location.x
+    cam_loc_y = cam.location.y
+    # The different radii range
+    #radius_range = range(3,9)
+    radius_range = [9]
+
+    R = (target.location.xy-cam.location.xy).length # Radius
+
+    init_angle  = (1-2*bool((cam_loc_y-t_loc_y)<0))*acos((cam_loc_x-t_loc_x)/R)-2*pi*bool((cam_loc_y-t_loc_y)<0) # 8.13 degrees
+
+    #can we get rid of the init angle stuff??`
+    target_angle = (2*pi - init_angle) # Go 90-8 deg more
+    #pi/2 is 90 degrees, 2pi is 360
+    num_steps = 20 #how many rotation steps
+
+    for r in radius_range:
+        for x in range(num_steps):
+            alpha = init_angle + (x)*target_angle/num_steps
+            cam.rotation_euler[2] = pi/2 + alpha #
+            cam.location.x = t_loc_x+cos(alpha)*r
+            cam.location.y = t_loc_y+sin(alpha)*r
+
+            # Define SAVEPATH and output filename
+
+            dir_path = os.path.join(save_folder, str(instance_id))
+            if not os.exists(dir_path):
+                os.mkdir(dir_path)
+            save_file = os.path.join(dir_path, str(x)+'_'+ str(r)+'_'+str(round(alpha,2))+'_'+str(round(cam.location.x, 2))+'_'+str(round(cam.location.y, 2)))
+
+            # Render
+            bpy.context.scene.render.filepath = save_file
+            bpy.ops.render.render(write_still=True)
+
+load_objs(paths[0])
+
+take_panorama(148, save_path)
+
+delete_objects()
