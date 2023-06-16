9867bdb1fd88c34dc0df966fdf79822c88190877
Ben Provan-Bessell <benjaminlpb123@gmail.com>
2023-06-16 12:41:48 +0100

    Add binary classification evaluation code

--- code/fault_detection/eval.py
+++ code/fault_detection/eval.py
@@ -14,7 +14,9 @@ def evaluate_binary(dataloader, model, device, set="Test"):
 
     # set up metrics
     acc_metric = torchmetrics.Accuracy(task='binary').to(device)
+    acc_sample_metric = torchmetrics.Accuracy(task='binary', multidim_average='samplewise').to(device)
     prec_metric = torchmetrics.Precision(task='binary', average='none').to(device)
+    confmat = torchmetrics.ConfusionMatrix(task="binary", num_classes=2).to(device)
 
     with torch.no_grad():
     # set the model in evaluation mode
@@ -27,14 +29,27 @@ def evaluate_binary(dataloader, model, device, set="Test"):
             # calculate the number of correct predictions
             # threshold of 0.5
             pred_class = torch.sigmoid(pred).round()
-            acc = acc_metric(torch.sigmoid(pred), y)
-            prec = prec_metric(torch.sigmoid(pred), y)
+            preds_sigmoid = torch.sigmoid(pred)
+            acc = acc_metric(preds_sigmoid, y)
+            # acc_sample = acc_sample_metric(preds_sigmoid, pred_class)
+            prec = prec_metric(preds_sigmoid, y)
+            conf = confmat(preds_sigmoid, y)
             # Correct += (pred_class == y).type(torch.float).sum().item()
 
         total_acc = acc_metric.compute()
         precision = prec_metric.compute()
+        confusion_matrix = confmat.compute()
+        # total_acc_sample = acc_sample_metric.compute()
 
         print("{} accuracy: {:.4f}, {} precision: {:.4f}".format(set, total_acc.item(), set, precision.item()))
+        # print("Accuracies per class: ", total_acc_sample)
+        # class 0 [True positive, False negative]
+        # class 1 [False Positive, True Negative]
+        class0_acc = confusion_matrix[0][0]/(confusion_matrix[0][1] + confusion_matrix[0][0])
+        class1_acc = confusion_matrix[1][1]/(confusion_matrix[1][1] + confusion_matrix[1][0])
+
+        print("Class 0 accuracy: ", class0_acc.item(), "Class 1 accuracy: ", class1_acc.item())
+        print("Confusion matrix: ", confusion_matrix)
 
         return total_acc
 
