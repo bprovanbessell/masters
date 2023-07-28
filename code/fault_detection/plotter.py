8605e0ead10dda2deed8b729cc13300a92734830
Ben Provan-Bessell <benjaminlpb123@gmail.com>
2023-07-28 13:13:44 +0100

    Add mean acc and precision to plotter

--- code/fault_detection/plotter.py
+++ code/fault_detection/plotter.py
@@ -3,7 +3,7 @@ Plot train vs validation loss and accuracies over epochs
 
 Anything else that needs to be done
 """
-
+import numpy as np
 import json
 
 class Plotter():
@@ -16,6 +16,8 @@ class Plotter():
 
 
 def plot_latex_table_rows(res_json):
+    accuracies = []
+    precisions = []
     with open(res_json, "r") as f:
         data: dict = json.load(f)
         categories = list(data.keys())
@@ -23,8 +25,15 @@ def plot_latex_table_rows(res_json):
         for category in categories:
             cat_res = data[category]
             print("{} & {:.4f}\%  & {:.4f} \\".format(category, cat_res['accuracy'] * 100, cat_res['precision']))
+            accuracies.append(cat_res['accuracy'] * 100)
+            precisions.append(cat_res['precision'])
+        print("{} & {:.4f}\%  & {:.4f} \\".format("Mean", np.mean(accuracies), np.mean(precisions)))
 
 
 if __name__ == "__main__":
     res_json = "/Users/bprovan/University/dissertation/masters/code/fault_detection/logs/baseline_binary.json"
+    res_json = "/Users/bprovan/University/dissertation/masters/code/fault_detection/logs/results/siamese_res.json"
+    res_json = "/Users/bprovan/University/dissertation/masters/code/fault_detection/logs/results/multiview_standard.json"
+
+
     plot_latex_table_rows(res_json)
